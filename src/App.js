738c7c7c6455fb567e0c136d6d305f10c29c2579
huy2012 <huynn@reactplus.jp>
2022-04-21 16:09:52 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ function App() {
                 <Route path={"/profile"} element={<Profile />}/>
                 <Route path={"/detailProduct"} element={<DetailProduct />}/>
             </Routes>
-{/*  */}
+
         </div>
     );
 }
